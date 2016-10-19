fddaca09a11a4f8a6e444b85a457a73650206325
bazzilic <bazzilic@gmail.com>
2016-10-20 00:35:00 +0800

    Edits to nuspec, AssemblyInfo, references

--- PrismaDB-QueryParser/Properties/AssemblyInfo.cs
+++ PrismaDB-QueryParser/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PrismaDB-QueryParser")]
+[assembly: AssemblyTitle("PrismaDB.QueryParser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PrismaDB-QueryParser")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Vasily Sidorov (bazzilic) / Nanyang Technological University")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
