ab20d4989801547f1cb68cd0ee8428f860ee4c50
Calvin Che <calvin@cczy.io>
2019-04-02 21:16:15 +0800

    expression classes now handle parents more appropriately

--- PrismaDB-QueryParser-MSSQL/Visitors/CommonVisitors.cs
+++ PrismaDB-QueryParser-MSSQL/Visitors/CommonVisitors.cs
@@ -251,18 +251,22 @@ namespace PrismaDB.QueryParser.MSSQL
 
         public override object VisitScalarFunctionCall([NotNull] MsSqlParser.ScalarFunctionCallContext context)
         {
+            ScalarFunction res;
             if (context.scalarFunctionName().SUM() != null)
-                return new SumAggregationFunction(context.scalarFunctionName().GetText(), parameters: (List<Expression>)Visit(context.functionArgs()));
+                res = new SumAggregationFunction(context.scalarFunctionName().GetText());
             else if (context.scalarFunctionName().COUNT() != null)
-                return new CountAggregationFunction(context.scalarFunctionName().GetText(), parameters: (List<Expression>)Visit(context.functionArgs()));
+                res = new CountAggregationFunction(context.scalarFunctionName().GetText());
             else if (context.scalarFunctionName().AVG() != null)
-                return new AvgAggregationFunction(context.scalarFunctionName().GetText(), parameters: (List<Expression>)Visit(context.functionArgs()));
+                res = new AvgAggregationFunction(context.scalarFunctionName().GetText());
             else if (context.scalarFunctionName().STDEV() != null)
-                return new StDevAggregationFunction(context.scalarFunctionName().GetText(), parameters: (List<Expression>)Visit(context.functionArgs()));
+                res = new StDevAggregationFunction(context.scalarFunctionName().GetText());
+            else
+                res = new ScalarFunction(context.scalarFunctionName().GetText());
 
-            var res = new ScalarFunction(context.scalarFunctionName().GetText());
             if (context.functionArgs() != null)
-                res.Parameters = (List<Expression>)Visit(context.functionArgs());
+                foreach (var exp in (List<Expression>)Visit(context.functionArgs()))
+                    res.AddChild(exp);
+
             return res;
         }
 
@@ -270,7 +274,8 @@ namespace PrismaDB.QueryParser.MSSQL
         {
             var res = new ScalarFunction((Identifier)Visit(context.uid()));
             if (context.functionArgs() != null)
-                res.Parameters = (List<Expression>)Visit(context.functionArgs());
+                foreach (var exp in (List<Expression>)Visit(context.functionArgs()))
+                    res.AddChild(exp);
             return res;
         }
 
